2751bf114ff5251d4e1c395fd08e999d9c0af713
mikeyrayvon <mikey@interglobal.vision>
2020-07-18 09:45:27 +0200

    improve shop menu styling

--- partials/shop-filter.php
+++ partials/shop-filter.php
@@ -6,66 +6,66 @@ $collections = get_terms_by_post_type( array('collection'), array('product') );
 <div id="shop-menu" class="background-pale border-bottom">
   <div class="container">
     <ul id="shop-menu-list" class="grid-row padding-top-tiny">
-      <li class="padding-bottom-tiny grid-item item-s-12 item-l-auto offset-l-6 shop-sub-menu-trigger">
-        <span>Mediums</span>
-        <div class="shop-sub-menu padding-top-small padding-bottom-tiny grid-row">
-          <ul class="shop-sub-menu-list grid-item item-s-12 item-l-6 offset-m-6 no-gutter grid-row">
+      <li class="shop-sub-menu-trigger padding-bottom-tiny grid-item item-s-12 item-l-auto offset-l-6">
+        <div class="shop-menu-label"><span>Mediums</span></div>
+        <div class="shop-sub-menu padding-bottom-tiny grid-row">
+          <ul class="shop-sub-menu-list grid-item item-s-12 item-l-6 offset-l-6 no-gutter grid-row">
           <?php
             foreach ($mediums as $term) {
               $params = array('filter' => 'medium', 'by' => $term->slug);
           ?>
-            <li class="shop-sub-menu-item grid-item item-s-12 item-l-3 margin-bottom-tiny"><a href="<?php echo add_query_arg($params); ?>"><?php echo $term->name; ?></a></li>
+            <li class="shop-sub-menu-item grid-item item-s-auto item-l-3 margin-bottom-tiny"><a href="<?php echo add_query_arg($params); ?>"><?php echo $term->name; ?></a></li>
           <?php } ?>
           </ul>
         </div>
       </li>
-      <li class="padding-bottom-tiny grid-item item-s-12 item-l-auto shop-sub-menu-trigger">
-        <span>Artists</span>
-        <div class="shop-sub-menu padding-top-small padding-bottom-tiny grid-row">
-          <ul class="shop-sub-menu-list grid-item item-s-12 item-l-6 offset-m-6 no-gutter grid-row">
+      <li class="shop-sub-menu-trigger padding-bottom-tiny grid-item item-s-12 item-l-auto">
+        <div class="shop-menu-label"><span>Artists</span></div>
+        <div class="shop-sub-menu padding-bottom-tiny grid-row">
+          <ul class="shop-sub-menu-list grid-item item-s-12 item-l-6 offset-l-6 no-gutter grid-row">
           <?php
             foreach ($artists as $term) {
               $params = array('filter' => 'artist', 'by' => $term->slug);
           ?>
-            <li class="shop-sub-menu-item grid-item item-s-12 item-l-3 margin-bottom-tiny"><a href="<?php echo add_query_arg($params); ?>"><?php echo $term->name; ?></a></li>
+            <li class="shop-sub-menu-item grid-item item-s-auto item-l-3 margin-bottom-tiny"><a href="<?php echo add_query_arg($params); ?>"><?php echo $term->name; ?></a></li>
           <?php } ?>
           </ul>
         </div>
       </li>
-      <li class="padding-bottom-tiny grid-item item-s-12 item-l-auto shop-sub-menu-trigger">
-        <span>Collections</span>
-        <div class="shop-sub-menu padding-top-small padding-bottom-tiny grid-row">
-          <ul class="shop-sub-menu-list grid-item item-s-12 item-l-6 offset-m-6 no-gutter grid-row">
+      <li class="shop-sub-menu-trigger padding-bottom-tiny grid-item item-s-12 item-l-auto">
+        <div class="shop-menu-label"><span>Collections</span></div>
+        <div class="shop-sub-menu padding-bottom-tiny grid-row">
+          <ul class="shop-sub-menu-list grid-item item-s-12 item-l-6 offset-l-6 no-gutter grid-row">
           <?php
             foreach ($collections as $term) {
               $params = array('filter' => 'collection', 'by' => $term->slug);
           ?>
-            <li class="shop-sub-menu-item grid-item item-s-12 item-l-3 margin-bottom-tiny"><a href="<?php echo add_query_arg($params); ?>"><?php echo $term->name; ?></a></li>
+            <li class="shop-sub-menu-item grid-item item-s-auto item-l-3 margin-bottom-tiny"><a href="<?php echo add_query_arg($params); ?>"><?php echo $term->name; ?></a></li>
           <?php } ?>
           </ul>
         </div>
       </li>
       <?php if (is_post_type_archive('product')) { ?>
-      <li class="padding-bottom-tiny grid-item item-s-12 item-l-auto shop-sub-menu-trigger">
-        <span>Sort By</span>
-        <div class="shop-sub-menu padding-top-small padding-bottom-tiny grid-row">
-          <ul class="shop-sub-menu-list grid-item item-s-12 item-l-6 offset-m-6 no-gutter grid-row">
-            <li class="shop-sub-menu-item grid-item item-s-12 item-l-3 margin-bottom-tiny">
+      <li class="shop-sub-menu-trigger padding-bottom-tiny grid-item item-s-12 item-l-auto">
+        <div class="shop-menu-label"><span>Sort By</span></div>
+        <div class="shop-sub-menu padding-bottom-tiny grid-row">
+          <ul class="shop-sub-menu-list grid-item item-s-12 item-l-6 offset-l-6 no-gutter grid-row">
+            <li class="shop-sub-menu-item grid-item item-s-auto item-l-3 margin-bottom-tiny">
               <button class="shop-sort-option active" data-sort="newest">Newest</button>
             </li>
-            <li class="shop-sub-menu-item grid-item item-s-12 item-l-3 margin-bottom-tiny">
+            <li class="shop-sub-menu-item grid-item item-s-auto item-l-3 margin-bottom-tiny">
               <button class="shop-sort-option" data-sort="editors-picks">Editor's Picks</button>
             </li>
-            <li class="shop-sub-menu-item grid-item item-s-12 item-l-3 margin-bottom-tiny">
+            <li class="shop-sub-menu-item grid-item item-s-auto item-l-3 margin-bottom-tiny">
               <button class="shop-sort-option" data-sort="low-high">Low to High</button>
             </li>
-            <li class="shop-sub-menu-item grid-item item-s-12 item-l-3 margin-bottom-tiny">
+            <li class="shop-sub-menu-item grid-item item-s-auto item-l-3 margin-bottom-tiny">
               <button class="shop-sort-option" data-sort="high-low">High to Low</button>
             </li>
-            <li class="shop-sub-menu-item grid-item item-s-12 item-l-3 margin-bottom-tiny">
+            <li class="shop-sub-menu-item grid-item item-s-auto item-l-3 margin-bottom-tiny">
               <button class="shop-sort-option" data-sort="under-500">Under $500</button>
             </li>
-            <li class="shop-sub-menu-item grid-item item-s-12 item-l-3 margin-bottom-tiny">
+            <li class="shop-sub-menu-item grid-item item-s-auto item-l-3 margin-bottom-tiny">
               <button class="shop-sort-option" data-sort="under-1000">Under $1000</button>
             </li>
           </ul>
