eec012bbe7ab56ff1c563ae080c3743060e7cfd5
mikeyrayvon <mikey@interglobal.vision>
2020-09-29 16:58:35 +0200

    loop through all weekly types on weekly page

--- page-weeklies.php
+++ page-weeklies.php
@@ -6,128 +6,56 @@ global $pale;
 
 <main id="main-content">
 <?php
-$top_weekly_args = array(
-  'post_type' => 'weekly',
-  'posts_per_page' => 1,
-  'tax_query' => array(
-    array(
-      'taxonomy' => 'weeklytype',
-      'field'    => 'slug',
-      'terms'    => 'studios',
+$weekly_types = get_terms(array(
+  'taxonomy' => 'weeklytype',
+  'orderby' => 'count',
+  'order' => 'DESC',
+));
+
+foreach ($weekly_types as $weekly_type) {
+  $top_weekly_args = array(
+    'post_type' => 'weekly',
+    'posts_per_page' => 1,
+    'tax_query' => array(
+      array(
+        'taxonomy' => 'weeklytype',
+        'field'    => 'slug',
+        'terms'    => $weekly_type->slug,
+      ),
     ),
-  ),
-);
+  );
 
-$top_weekly_query = new WP_Query($top_weekly_args);
-if ($top_weekly_query->have_posts()) {
-  while ($top_weekly_query->have_posts()) {
-    $top_weekly_query->the_post();
+  $top_weekly_query = new WP_Query($top_weekly_args);
+  if ($top_weekly_query->have_posts()) {
+    while ($top_weekly_query->have_posts()) {
+      $top_weekly_query->the_post();
 
-    get_template_part('partials/article-full-item');
+      get_template_part('partials/article-full-item');
+    }
   }
+
+  wp_reset_postdata();
+
+  $weeklies_args = array(
+    'post_type' => 'weekly',
+    'posts_per_page' => 10,
+    'offset' => 1,
+    'tax_query' => array(
+      array(
+        'taxonomy' => 'weeklytype',
+        'field'    => 'slug',
+        'terms'    => $weekly_type->slug,
+      ),
+    ),
+  );
+  $weeklies_section_title = $weekly_type->name;
+  global $weeklies_args;
+  global $weeklies_section_title;
+
+  get_template_part('partials/weeklies');
+
+  wp_reset_postdata();
 }
-
-wp_reset_postdata();
-
-$weeklies_args = array(
-  'post_type' => 'weekly',
-  'posts_per_page' => 10,
-  'offset' => 1,
-  'tax_query' => array(
-    array(
-      'taxonomy' => 'weeklytype',
-      'field'    => 'slug',
-      'terms'    => 'studios',
-    ),
-  ),
-);
-$weeklies_section_title = 'So-Far Studios';
-global $weeklies_args;
-global $weeklies_section_title;
-
-get_template_part('partials/weeklies');
-
-$top_weekly_args = array(
-  'post_type' => 'weekly',
-  'posts_per_page' => 1,
-  'tax_query' => array(
-    array(
-      'taxonomy' => 'weeklytype',
-      'field'    => 'slug',
-      'terms'    => 'reviews',
-    ),
-  ),
-);
-
-$top_weekly_query = new WP_Query($top_weekly_args);
-if ($top_weekly_query->have_posts()) {
-  while ($top_weekly_query->have_posts()) {
-    $top_weekly_query->the_post();
-
-    get_template_part('partials/article-full-item');
-  }
-}
-
-wp_reset_postdata();
-
-$weeklies_args = array(
-  'post_type' => 'weekly',
-  'posts_per_page' => 10,
-  'offset' => 1,
-  'tax_query' => array(
-    array(
-      'taxonomy' => 'weeklytype',
-      'field'    => 'slug',
-      'terms'    => 'reviews',
-    ),
-  ),
-);
-$weeklies_section_title = 'So-Far Reviews';
-global $weeklies_args;
-global $weeklies_section_title;
-
-get_template_part('partials/weeklies');
-
-$top_weekly_args = array(
-  'post_type' => 'weekly',
-  'posts_per_page' => 1,
-  'tax_query' => array(
-    array(
-      'taxonomy' => 'weeklytype',
-      'field'    => 'slug',
-      'terms'    => 'friendships',
-    ),
-  ),
-);
-
-$top_weekly_query = new WP_Query($top_weekly_args);
-if ($top_weekly_query->have_posts()) {
-  while ($top_weekly_query->have_posts()) {
-    $top_weekly_query->the_post();
-
-    get_template_part('partials/article-full-item');
-  }
-}
-
-wp_reset_postdata();
-
-$weeklies_args = array(
-  'post_type' => 'weekly',
-  'posts_per_page' => 10,
-  'offset' => 1,
-  'tax_query' => array(
-    array(
-      'taxonomy' => 'weeklytype',
-      'field'    => 'slug',
-      'terms'    => 'friendships',
-    ),
-  ),
-);
-$weeklies_section_title = 'So-Far Friendships';
-global $weeklies_args;
-global $weeklies_section_title;
-
-get_template_part('partials/weeklies');
 ?>
 
   <section class="padding-bottom-basic">
