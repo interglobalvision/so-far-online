d7cce6423da99cc3f1c3ff9da8deee30e3ecb766
mikeyrayvon <mikey@interglobal.vision>
2020-10-14 23:37:45 +0200

    improve 404

--- 404.php
+++ 404.php
@@ -3,8 +3,11 @@ get_header();
 ?>
 
 <section id="main-content">
-  <h1>Not found</h1>
-  <p>:(</p>
+  <div class="grid-row justify-center">
+    <div class="grid-item">
+      <h1>Not found</h1>
+    </div>
+  </div>
 </section>
 
 <?php
