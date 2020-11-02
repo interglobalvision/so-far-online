af8fb2a85da2ef27bfd0f00dd16785c8b2f40907
mikeyrayvon <mikey@interglobal.vision>
2020-11-02 19:59:45 +0100

    fix shop filter styling

--- partials/shop-filter.php
+++ partials/shop-filter.php
@@ -5,11 +5,11 @@ $collections = get_terms_by_post_type( array('collection'), array('product') );
 $is_shop_archive = is_post_type_archive('product');
 $shop_archive_url = get_post_type_archive_link('product');
 ?>
-<div id="shop-menu" class="background-pale">
-  <div id="shop-menu-toggle" class="not-desktop border-bottom text-align-center padding-top-tiny padding-bottom-tiny background-pale u-pointer">
+<div id="shop-menu" class="background-white">
+  <div id="shop-menu-toggle" class="not-desktop border-bottom text-align-center padding-top-tiny padding-bottom-tiny background-white u-pointer">
     <span class="font-bold font-size-mid font-uppercase">Shop Menu</span>
   </div>
-  <div id="shop-menu-holder" class="border-bottom background-pale">
+  <div id="shop-menu-holder" class="border-bottom background-white">
     <div class="container">
       <div class="not-desktop"><?php get_search_form() ?></div>
       <ul id="shop-menu-list" class="grid-row padding-top-tiny justify-center">
@@ -18,7 +18,7 @@ $shop_archive_url = get_post_type_archive_link('product');
         </li>
         <li class="shop-sub-menu-trigger padding-bottom-tiny grid-item item-s-12 item-l-auto">
           <div class="shop-menu-label"><span>Mediums</span></div>
-          <div class="shop-sub-menu padding-bottom-tiny grid-row justify-center">
+          <div class="shop-sub-menu padding-bottom-tiny grid-row justify-center background-white">
             <div class="grid-item item-l-6">&nbsp;</div>
             <ul class="shop-sub-menu-list grid-item item-s-12 item-l-6 offset-l-6 no-gutter grid-row">
             <?php
@@ -33,7 +33,7 @@ $shop_archive_url = get_post_type_archive_link('product');
         </li>
         <li class="shop-sub-menu-trigger padding-bottom-tiny grid-item item-s-12 item-l-auto">
           <div class="shop-menu-label"><span>Artists</span></div>
-          <div class="shop-sub-menu padding-bottom-tiny grid-row justify-center">
+          <div class="shop-sub-menu padding-bottom-tiny grid-row justify-center background-white">
             <div class="grid-item item-l-6">&nbsp;</div>
             <ul class="shop-sub-menu-list grid-item item-s-12 item-l-6 offset-l-6 no-gutter grid-row">
             <?php
@@ -48,7 +48,7 @@ $shop_archive_url = get_post_type_archive_link('product');
         </li>
         <li class="shop-sub-menu-trigger padding-bottom-tiny grid-item item-s-12 item-l-auto">
           <div class="shop-menu-label"><span>Collections</span></div>
-          <div class="shop-sub-menu padding-bottom-tiny grid-row justify-center">
+          <div class="shop-sub-menu padding-bottom-tiny grid-row justify-center background-white">
             <div class="grid-item item-l-6">&nbsp;</div>
             <ul class="shop-sub-menu-list grid-item item-s-12 item-l-6 offset-l-6 no-gutter grid-row">
             <?php
@@ -64,7 +64,7 @@ $shop_archive_url = get_post_type_archive_link('product');
         <?php if (is_post_type_archive('product')) { ?>
         <li class="shop-sub-menu-trigger padding-bottom-tiny grid-item item-s-12 item-l-auto">
           <div class="shop-menu-label"><span>Sort By</span></div>
-          <div class="shop-sub-menu padding-bottom-tiny grid-row justify-center">
+          <div class="shop-sub-menu padding-bottom-tiny grid-row justify-center background-white">
             <ul class="shop-sub-menu-list grid-item item-s-12 item-l-6 offset-l-6 no-gutter grid-row">
               <li class="shop-sub-menu-item grid-item item-s-auto item-l-3 margin-bottom-tiny">
                 <button class="shop-sort-option active" data-sort="newest">Newest</button>
