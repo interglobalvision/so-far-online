ce2514ab3f3f5050bf970a216a513563b4e1a63b
mikeyrayvon <mikey@interglobal.vision>
2020-11-08 15:30:09 +0100

    style social icons

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
 $options = get_site_option('_igv_site_options');
 ?>
 
-  <footer id="footer" class="font-size-small">
+  <footer id="footer" class="font-size-basic">
     <div class="container">
       <div class="grid-row">
         <?php if (!empty($options['mailchimp_action'])) { ?>
@@ -12,7 +12,7 @@ $options = get_site_option('_igv_site_options');
         <?php } ?>
 
         <div id="footer-nav-holder" class="item-s-12 item-m-6 offset-xl-1 grid-column justify-between">
-          <div id="footer-nav" class="margin-bottom-tiny">
+          <div id="footer-nav">
             <ul class="grid-row">
               <li class="grid-item item-s-6 item-l-3 padding-bottom-tiny">
                 <a href="<?php echo home_url('About'); ?>">About</a>
@@ -40,33 +40,33 @@ $options = get_site_option('_igv_site_options');
 
           <div class="grid-row align-items-center">
             <div class="grid-item item-s-auto">
-              Connect with Us
+              <span class="font-size-small">Connect with us on:</span>
             </div>
             <ul class="grid-row item-s-auto padding-top-micro font-size-zero">
             <?php if (!empty($options['socialmedia_instagram'])) { ?>
               <li class="grid-item">
-                <a href="https://instagram.com/<?php echo $options['socialmedia_instagram']; ?>">
+                <a href="https://instagram.com/<?php echo $options['socialmedia_instagram']; ?>" class="social-icon-holder">
                   <img class="social-icon" src="<?php bloginfo('stylesheet_directory'); ?>/dist/img/icon-instagram.png" />
                   <img class="social-icon social-icon-white" src="<?php bloginfo('stylesheet_directory'); ?>/dist/img/icon-instagram-white.png" />
                 </a>
               </li>
             <?php } if (!empty($options['socialmedia_facebook'])) { ?>
               <li class="grid-item">
-                <a href="https://facebook.com/<?php echo $options['socialmedia_facebook']; ?>">
+                <a href="https://facebook.com/<?php echo $options['socialmedia_facebook']; ?>" class="social-icon-holder">
                   <img class="social-icon" src="<?php bloginfo('stylesheet_directory'); ?>/dist/img/icon-facebook.png" />
                   <img class="social-icon social-icon-white" src="<?php bloginfo('stylesheet_directory'); ?>/dist/img/icon-facebook-white.png" />
                 </a>
               </li>
             <?php } if (!empty($options['socialmedia_twitter'])) { ?>
               <li class="grid-item">
-                <a href="https://twitter.com/<?php echo $options['socialmedia_twitter']; ?>">
+                <a href="https://twitter.com/<?php echo $options['socialmedia_twitter']; ?>" class="social-icon-holder">
                   <img class="social-icon" src="<?php bloginfo('stylesheet_directory'); ?>/dist/img/icon-twitter.png" />
                   <img class="social-icon social-icon-white" src="<?php bloginfo('stylesheet_directory'); ?>/dist/img/icon-twitter-white.png" />
                 </a>
               </li>
             <?php } if (!empty($options['socialmedia_vimeo'])) { ?>
               <li class="grid-item">
-                <a href="https://vimeo.com/<?php echo $options['socialmedia_vimeo']; ?>">
+                <a href="https://vimeo.com/<?php echo $options['socialmedia_vimeo']; ?>" class="social-icon-holder">
                   <img class="social-icon" src="<?php bloginfo('stylesheet_directory'); ?>/dist/img/icon-vimeo.png" />
                   <img class="social-icon social-icon-white" src="<?php bloginfo('stylesheet_directory'); ?>/dist/img/icon-vimeo-white.png" />
                 </a>
