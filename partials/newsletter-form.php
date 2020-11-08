c913cfd31e08b11f6ff14b20a31f8cf8b316d7cb
mikeyrayvon <mikey@interglobal.vision>
2020-11-08 15:07:36 +0100

    change newsletter placeholders

--- partials/newsletter-form.php
+++ partials/newsletter-form.php
@@ -5,10 +5,10 @@ if (!empty($options['mailchimp_action'])) {
 <form novalidate="true" id="mailchimp-form" class="text-align-center">
   <div class="margin-bottom-micro grid-row">
     <div class="grid-item item-s-6">
-      <input type="name" name="FNAME" placeholder="First Name" id="mailchimp-first">
+      <input type="name" name="FNAME" placeholder="Name" id="mailchimp-first">
     </div>
     <div class="grid-item item-s-6">
-      <input type="name" name="LNAME" placeholder="Last Name" id="mailchimp-last">
+      <input type="name" name="LNAME" placeholder="Surname" id="mailchimp-last">
     </div>
   </div>
   <div class="grid-row align-items-center">
