00736591f94c797ab8213b393506c1dde00449ad
mikeyrayvon <mikey@interglobal.vision>
2020-11-10 18:08:39 +0100

    reorder footer nav

--- footer.php
+++ footer.php
@@ -15,22 +15,22 @@ $options = get_site_option('_igv_site_options');
           <div id="footer-nav">
             <ul class="grid-row">
               <li class="grid-item item-s-6 item-l-4 padding-bottom-tiny">
-                <a href="<?php echo home_url('contact'); ?>">Contact</a>
-              </li>
-              <li class="grid-item item-s-6 item-l-4 padding-bottom-tiny">
                 <a href="<?php echo home_url('definitions'); ?>">Definitions</a>
               </li>
               <li class="grid-item item-s-6 item-l-4 padding-bottom-tiny">
                 <a href="<?php echo home_url('contribute'); ?>">Contribute</a>
               </li>
               <li class="grid-item item-s-6 item-l-4 padding-bottom-tiny">
+                <a href="<?php echo home_url('contact'); ?>">Contact</a>
+              </li>
+              <li class="grid-item item-s-6 item-l-4 padding-bottom-tiny">
                 <a href="<?php echo home_url('faq'); ?>">FAQ</a>
               </li>
               <li class="grid-item item-s-6 item-l-4 padding-bottom-tiny">
-                <a href="<?php echo home_url('privacy-policy'); ?>">Privacy Policy</a>
+                <a href="<?php echo home_url('terms-and-conditions'); ?>">Terms & Conditions</a>
               </li>
               <li class="grid-item item-s-6 item-l-4 padding-bottom-tiny">
-                <a href="<?php echo home_url('terms-and-conditions'); ?>">Terms & Conditions</a>
+                <a href="<?php echo home_url('privacy-policy'); ?>">Privacy Policy</a>
               </li>
             </ul>
           </div>
