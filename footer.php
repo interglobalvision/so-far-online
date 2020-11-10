322ffb04ebc5857b4a3ffde2bff9e3248c8bdd1a
mikeyrayvon <mikey@interglobal.vision>
2020-11-10 17:55:54 +0100

    remove About from footer nav

--- footer.php
+++ footer.php
@@ -14,25 +14,22 @@ $options = get_site_option('_igv_site_options');
         <div id="footer-nav-holder" class="item-s-12 item-m-6 offset-xl-1 grid-column justify-between">
           <div id="footer-nav">
             <ul class="grid-row">
-              <li class="grid-item item-s-6 item-l-3 padding-bottom-tiny">
-                <a href="<?php echo home_url('About'); ?>">About</a>
-              </li>
-              <li class="grid-item item-s-6 item-l-3 padding-bottom-tiny">
+              <li class="grid-item item-s-6 item-l-4 padding-bottom-tiny">
                 <a href="<?php echo home_url('contact'); ?>">Contact</a>
               </li>
-              <li class="grid-item item-s-6 item-l-3 padding-bottom-tiny">
+              <li class="grid-item item-s-6 item-l-4 padding-bottom-tiny">
                 <a href="<?php echo home_url('definitions'); ?>">Definitions</a>
               </li>
-              <li class="grid-item item-s-6 item-l-3 padding-bottom-tiny">
+              <li class="grid-item item-s-6 item-l-4 padding-bottom-tiny">
                 <a href="<?php echo home_url('contribute'); ?>">Contribute</a>
               </li>
-              <li class="grid-item item-s-6 item-l-3 padding-bottom-tiny">
+              <li class="grid-item item-s-6 item-l-4 padding-bottom-tiny">
                 <a href="<?php echo home_url('faq'); ?>">FAQ</a>
               </li>
-              <li class="grid-item item-s-6 item-l-3 padding-bottom-tiny">
+              <li class="grid-item item-s-6 item-l-4 padding-bottom-tiny">
                 <a href="<?php echo home_url('privacy-policy'); ?>">Privacy Policy</a>
               </li>
-              <li class="grid-item item-s-6 item-l-3 padding-bottom-tiny">
+              <li class="grid-item item-s-6 item-l-4 padding-bottom-tiny">
                 <a href="<?php echo home_url('terms-and-conditions'); ?>">Terms & Conditions</a>
               </li>
             </ul>
