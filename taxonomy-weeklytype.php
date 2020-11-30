0a1b262b9e1173011b922f74e062670fcf4efa7f
mikeyrayvon <mikey@interglobal.vision>
2020-11-30 11:21:38 +0100

    add missing margin

--- taxonomy-weeklytype.php
+++ taxonomy-weeklytype.php
@@ -36,7 +36,7 @@ if (have_posts()) {
               </div>
             </div>
             <h2 class="grid-item item-s-12 font-size-mid margin-bottom-micro"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-            <div class="grid-item item-s-10"><span><?php echo !empty($subtitle) ? $subtitle : ''; ?></span></div>
+            <div class="grid-item item-s-10 margin-bottom-micro"><span><?php echo !empty($subtitle) ? $subtitle : ''; ?></span></div>
             <div class="grid-item item-s-6 font-size-small font-uppercase font-heavy font-color-blush"><span><span><?php echo $type ? $type : ''; ?></span></span></div>
           </div>
         </article>
