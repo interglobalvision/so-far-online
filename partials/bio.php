c33c3883bf0027523cef1b21e42b20ee1e643e14
mikeyrayvon <mikey@interglobal.vision>
2021-11-09 19:59:50 +0100

    add term link around bio name on desktop

--- partials/bio.php
+++ partials/bio.php
@@ -13,7 +13,7 @@ $bio_photo_id = get_term_meta($bio->term_id, '_igv_bio_photo_id', true);
   </div>
   <div class="grid-item item-s-12 item-l-5">
     <div class="desktop-only font-serif">
-      <div><span class="font-size-mid"><?php echo $bio->name; ?></span></div>
+      <div><span class="font-size-mid"><a href="<?php echo get_term_link($bio->term_id); ?>"><?php echo $bio->name; ?></a></span></div>
       <div><span><?php echo !empty($birth_year) ? 'b. ' . $birth_year : ''; ?></span></div>
     </div>
     <div class="padding-top-tiny">
