2f18d0879eeeaf691c542860a2358e4722f33033
mikeyrayvon <mikey@interglobal.vision>
2021-12-09 10:53:42 +0100

    fix product item sold out flag

--- lib/functions-product.php
+++ lib/functions-product.php
@@ -3,19 +3,21 @@ function productPrice($post_id, $priceClass = '') {
   $price = get_post_meta($post_id, '_igv_product_price', true);
   $inventory = get_post_meta($post_id, '_igv_product_inventory', true);
 
-  if (!empty($inventory) && !empty($price)) {
-    if ($inventory > 0) {
-  ?>
-    <div class="product-price <?php echo $priceClass; ?>">
-      <span>$<?php echo $price; ?></span>
-    </div>
-  <?php 
-    } else {
-  ?> 
-    <div class="product-sold">
-      <span>Sold</span>
-    </div>
-  <?php 
-    }
-  }
+  if ($inventory !== '' && intval($inventory) < 1) {
+?> 
+
+<div class="product-sold">
+  <span>Sold</span>
+</div>
+
+<?php 
+  } else if (!empty($price)) {
+?>
+
+<div class="product-price <?php echo $priceClass; ?>">
+  <span>$<?php echo $price; ?></span>
+</div>
+
+<?php 
+  } 
 }
